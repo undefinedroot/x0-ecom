468e71f16623563b97a9ecd953bc77b49a9cd52d
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-10-14 13:45:40 +0800

    React-Bootstrap using NavBars, and initial theming

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './bootstrap.min.css'
 import './index.css';
 import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
