19e1c7b330a5e78e7cebb515b8d2e16a4d84d828
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-10-21 17:28:17 +0800

    react+redux setup, creation of actions and reducers for product list logic

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
