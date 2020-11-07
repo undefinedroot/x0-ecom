4726f5d19f7de53afeecc8c2c8c865e9036317aa
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-11-07 18:34:46 +0800

    final changes before heroku deployment.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,7 +28,7 @@ const HomeScreen = ({match}) => {
   return (
     <>
       <Meta />
-      {!keyword && <ProductCarousel />} : <Link to='/' className='btn btn-light'>Go Back</Link>
+      {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
       <h1>Latest Products</h1>
       {loading
         ? <Loader />
